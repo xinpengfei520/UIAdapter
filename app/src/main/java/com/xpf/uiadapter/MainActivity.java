9cd2d428f579cbdc8dddeea5871a4dd6871a0920
xinpengfei <xinpengfei@anloq.com>
2018-01-30 11:44:28 +0800

    first commit.

---
+++ app/src/main/java/com/xpf/uiadapter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xpf.uiadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
